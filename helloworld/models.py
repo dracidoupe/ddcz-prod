380c83ab55e5683bf07459861366c2c4759d3be8
Almad <bugs@almad.net>
2020-03-27 12:20:23 +0100

    feat: Dump objects

--- helloworld/models.py
+++ helloworld/models.py
@@ -3,3 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ZldMain(models.Model):
+    rocnik = models.CharField(unique=True, max_length=6)
+    status = models.CharField(max_length=1)
+
+    class Meta:
+        managed = False
+        db_table = 'zld_main'
--- helloworld/views.py
+++ helloworld/views.py
@@ -2,7 +2,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from .models import ZldMain
+
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+        return str([[o.rocnik, o.status] for o in ZldMain.objects.all())
