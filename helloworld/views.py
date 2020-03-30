e58a8e2f2439c072d629fcc251b989ac47b877e5
Almad <bugs@almad.net>
2020-03-30 16:13:05 +0200

    fix: Debugging on production

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,5 +1,6 @@
 from json import dumps
 
+from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
@@ -14,6 +15,8 @@ class HomePageView(View):
         try:
             data = dumps([[o.rocnik, o.status] for o in ZldMain.objects.all()])
         except OperationalError:
+            if settings.DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
+                raise
             data = dumps([])
 
         return HttpResponse(data, content_type='application/json')
